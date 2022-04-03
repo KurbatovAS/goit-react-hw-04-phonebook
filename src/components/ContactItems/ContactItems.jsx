63ac22fb6b60536110cee294450b5a6527c54365
unknown <nonamegod@gmail.com>
2022-04-03 19:36:12 +0300

    ContactItems moved to a separate component

---
+++ src/components/ContactItems/ContactItems.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import s from './ContactItems.module.css';
+
+function ContactItems ({contact, clickHandler}) {
+    console.log('contact', contact);
+    const {name, number} = contact;
+
+    return (
+        <li className={s.item}>
+            {name}: {number}
+            <button
+              type="button"
+              name={name}
+              className={s.button}
+              onClick={clickHandler}
+            >
+              Delete
+            </button>
+          </li>
+    )
+}
+
+ContactItems.propTypes = {
+    contact: PropTypes.shape({        
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+    }).isRequired,
+    clickHandler: PropTypes.func.isRequired,
+}
+
+export default ContactItems;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,13 @@
 import PropTypes from 'prop-types';
 import s from './ContactList.module.css';
+import ContactItems from '../ContactItems';
 
 function ContactList({ contacts, removeHandler }) {
   return (
     <ul className={s.list}>
       {contacts.map(contact => {
         return (
-          <li key={contact.id} className={s.item}>
-            {contact.name}: {contact.number}
-            <button
-              type="button"
-              name={contact.name}
-              className={s.button}
-              onClick={removeHandler}
-            >
-              Delete
-            </button>
-          </li>
+          <ContactItems key={contact.id} contact={contact} clickHandler={removeHandler} />
         );
       })}
     </ul>
@@ -26,9 +17,7 @@ function ContactList({ contacts, removeHandler }) {
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,  
     }).isRequired
   ).isRequired,
   removeHandler: PropTypes.func.isRequired,
