e1cd87bf5d4c772260af92818c24b1b6986ca3fb
unknown <nonamegod@gmail.com>
2022-04-03 14:23:16 +0300

    fix/add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import shortid from 'shortid';
 import Section from './Section/Section';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import Notitfication from './Notitfication';
 import ContactList from './ContactList';
+import {
+  getContactsFromLS,
+  addContactsToLS,
+} from './LocalStorage/LocalStorage';
 
-function App() {
-  const [contacts, setContacts] = useState(()=>([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]));
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+function App () {
+  const [contacts, setContacts] = useState(() => {
+    if (getContactsFromLS('contacts')) {
+      return getContactsFromLS('contacts');
+    } else {
+      return initialContacts;
+    }
+  });
   const [filter, setFilter] = useState('');
-  
-  const submitHandler = (newContactName, newContactNumber) => {
+
+  useEffect(() => {
+    addContactsToLS('contacts', contacts);
+  }, [contacts]) 
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const newContactName = e.target.elements.name.value;
+    const newContactNumber = e.target.elements.number.value;
+
     if (contactСheck(newContactName)) {
       alert(`${newContactName} is already in you contacts`);
       return;
@@ -27,8 +48,14 @@ function App() {
       number: newContactNumber,
     };
 
-    addNewContact(newContact);    
+    addNewContact(newContact);
+
+    e.target.reset();
   };
+
+  function addNewContact(newContact) {
+    setContacts(prev => [...prev, newContact])
+  }
 
   function contactСheck(name) {
     return contacts.find(
@@ -36,21 +63,13 @@ function App() {
     );
   }
 
-  function addNewContact(newContact) {
-    setContacts(prevState => [...prevState, newContact]    
-    );
-  }
-
   const onFilterInputHandler = e => {
-    setFilter(e.target.value.toLowerCase());
+    setFilter(e.target.value.toLowerCase())
   };
 
   function contactsFiltering() {
-    const contactList = contacts;
-    const filterValue = filter;
-
-    return contactList.filter(contact =>
-      contact.name.toLowerCase().includes(filterValue)
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
     );
   }
 
@@ -58,43 +77,37 @@ function App() {
     const contactToRemove = e.target.name;
     const contactIndex = findContactIndex(contactToRemove);
 
-    setContacts(prevState => 
-      prevState.filter(
-        (contact, index) => index !== contactIndex
-      ),
-    );
+    setContacts(prev => prev.filter(
+      (contact, index) => index !== contactIndex
+    ))    
   };
 
   function findContactIndex(contact) {
-    const contactList = contacts;
-
-    return contactList.findIndex(item => item.name === contact);
+    return contacts.findIndex(item => item.name === contact);
   }
 
-  const contactsLength = contacts.length;
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm onFormSubmit={submitHandler} />
+        </Section>
 
-  return (
-    <>
-      <Section title="Phonebook">
-        <ContactForm onFormSubmit={submitHandler} />
-      </Section>
-
-      <Section title="Contacts">
-        <Filter
-          onFilterInputHandler={onFilterInputHandler}
-          filterValue={filter}
-        />
-        {!contactsLength ? (
-          <Notitfication message="There is no contacts in you contact list" />
-        ) : (
-          <ContactList
-            contacts={contactsFiltering()}
-            removeHandler={removeContactHandler}
+        <Section title="Contacts">
+          <Filter
+            onFilterInputHandler={onFilterInputHandler}
+            filterValue={filter}
           />
-        )}
-      </Section>
-    </>
-  );
+          {!contacts.length ? (
+            <Notitfication message="There is no contacts in you contact list" />
+          ) : (
+            <ContactList
+              contacts={contactsFiltering()}
+              removeHandler={removeContactHandler}
+            />
+          )}
+        </Section>
+      </>
+    );
 }
 
 export default App;
